64eda9ffb0c6bc42aa42d372156e871323324498
Roman Popov <52584750+happy-roman@users.noreply.github.com>
2021-08-07 02:07:06 +1200

    version 0.1.3.8 add autoplay refactor hand switch control fix for more flexible uses

--- js/class-slider.js
+++ js/class-slider.js
@@ -3,7 +3,11 @@ const settings = {
   slides: '.time-line__item',
   place: '.time-line__brake-points',
   control: '.controle-timeline',
-  points: '.time-line__brake-point',
+  pointsClass: '.time-line__brake-point',
+  carousel: '.time-line__items',
+  autoPlay: true,
+  interval: 7000,
+  handSwitch: true
 }
 
 class Slider {
@@ -11,35 +15,49 @@ class Slider {
     this.slides = document.querySelectorAll(settings.slides)
     this.placeForRender = document.querySelector(settings.place)
     this.control = document.querySelector(settings.control)
+    this.carousel = document.querySelector(settings.carousel)
     this.points = ''
     this.point = 1
-    this._pointsRender()
-    this._getPoints(settings.points)
+    this.interval = 5000
+    this.pointClass = settings.pointsClass.split('.')[1]
+    this.itemClass = settings.slides.split('.')[1]
+    this._init(this.pointClass)
   }
 
-  _pointsRender() {
-    this.placeForRender.insertAdjacentHTML("afterbegin", this._setPoints())
+  _init(pointClass) {
+
+    this._pointsRender(pointClass)
+    this._getPoints(settings.pointsClass)
+    if (settings.autoPlay) {
+      this.interval = settings.interval
+      this._autoPlay()
+    }
+    settings.handSwitch ? this._handSwitch(pointClass) : null
   }
 
-  _setPoints() {
-    let timeLinePoints = ''
+  _pointsRender(pointClass) {
+    this.placeForRender.insertAdjacentHTML("afterbegin", this._setPoints(pointClass))
+  }
+
+  _setPoints(pointClass) {
+    let points = ''
     this.slides.forEach(el => {
-      let start = (this.point === 1) ? 'time-line__brake-point-active' : ''
-      timeLinePoints += `<div class="time-line__brake-point time-line__brake-point-${this.point} ${start}">${this.point}</div>`
+      let start = (this.point === 1) ? `${pointClass}-active` : ''
+      points += `<div class="${pointClass} ${pointClass}-${this.point} ${start}">${this.point}</div>`
       this.point++
     })
     this.point = 1
-    return timeLinePoints
+    return points
   }
 
-  _getPoints(points) {
-    return this.points = document.querySelectorAll(points)
+  _getPoints(pointsClass) {
+    return this.points = document.querySelectorAll(pointsClass)
   }
 
   _getPoint(target) {
     if (target !== '')
       for (let i = 1; i <= this.points.length; i++) {
-        const nameClass = 'time-line__brake-point-' + i
+        const nameClass = `${this.pointClass}-${i}`
         if (target.classList.contains(nameClass)) {
           this.point = i
         }
@@ -52,40 +70,49 @@ class Slider {
 
   _addActivePoint(point) {
     this._removeActivePoint()
-    const nameClass = 'time-line__brake-point-' + point
+    const nameClass = `${this.pointClass}-${point}`
     this.points.forEach((el) => {
       if (el.classList.contains(nameClass)) {
-        el.classList.add('time-line__brake-point-active')
+        el.classList.add(`${this.pointClass}-active`)
       }
     })
   }
 
   _removeActivePoint() {
     this.points.forEach(el => {
-      if (el.classList.contains('time-line__brake-point-active')) {
-        el.classList.remove('time-line__brake-point-active')
+      if (el.classList.contains(`${this.pointClass}-active`)) {
+        el.classList.remove(`${this.pointClass}-active`)
       }
     })
   }
 
   goToSlide(target = '') {
     const number = this._getPoint(target)
-    const targetSlide = 'time-line__item-' + number
+    const targetSlide = `${this.itemClass}-${number}`
     this._addActivePoint(this.point)
     for (let slide of this.slides) {
       if (slide.classList.contains(targetSlide)) {
-        slide.classList.add('time-line__item-active')
+        slide.classList.add(`${this.itemClass}-active`)
       } else {
-        slide.classList.remove('time-line__item-active')
+        slide.classList.remove(`${this.itemClass}-active`)
       }
     }
+  }
+
+  _autoPlay(interval = this.interval, carousel = this.carousel) {
+    let intervalID = setInterval(() => this.goToSlide(), interval)
+    const autoPlay = () => intervalID = setInterval(() => slider.goToSlide(), interval)
+    carousel.addEventListener('mouseover', () => clearInterval(intervalID))
+    carousel.addEventListener('mouseleave', () => autoPlay())
+  }
+
+  _handSwitch(pointClass) {
+    document.addEventListener('click', (event) => {
+      if (event.target.classList.contains(pointClass)) {
+        this.goToSlide(event.target)
+      }
+    })
   }
 }
 
 const slider = new Slider()
-setInterval(() => slider.goToSlide(), 10000)
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('time-line__brake-point')) {
-    slider.goToSlide(event.target)
-  }
-})
